4d78670e0fb4be215e9b1771e4dc398df3560330
1ozturkbe <1ozturkbe@gmail.com>
2017-11-02 11:30:02 -0400

    Engine fits completed.

--- enginefit/fitEnginePvsW.py
+++ enginefit/fitEnginePvsW.py
@@ -16,16 +16,17 @@ P = df[u'37000'];
 minP = amin(P)
 maxP = amax(P)
 logP = log(P/maxP)
-indP0 = logP.argmax()
+indP0 = np.array(np.argpartition(logP, -2)[-2:])
 
 W = df[u'33.2'];
 minW = amin(W)
 maxW = amax(W)
 logW = log(W/maxW)
-indW0 = logW.argmax()
+indW0 = np.array([logW.argmax()])
 
 
-indices = np.delete(np.linspace(0,len(logP)-1,len(logP)),[indP0,indW0])
+indices = np.delete(np.linspace(0,len(logP)-1,len(logP)),
+	np.concatenate([indP0,indW0]))
 logPin = np.array(logP[indices])
 logWin = np.array(logW[indices])
 
@@ -33,3 +34,17 @@ Type = 'SMA'
 K = 2
 
 cstrt, rms_error = fit(logPin,logWin,K,Type)
+#Result: w**0.789 = 0.0336 * (u_1)**0.157
+#           + 1.57 * (u_1)**1.34
+# rms_error 0.346
+
+
+# Plotting data
+plt.plot(P[indices]/1000,W[indices],'r*') 
+Pplot = np.linspace(1,150000,100)
+plt.plot(Pplot/1000,(0.0336 * (Pplot/maxP)**0.157
+    + 1.57 * (Pplot/maxP)**1.34)**(1/0.789)*maxW,'b-')
+plt.xlabel('Mean sea level maximum power (kW)')
+plt.ylabel('Engine weight (lbs)')
+plt.grid(True)
+plt.show()
