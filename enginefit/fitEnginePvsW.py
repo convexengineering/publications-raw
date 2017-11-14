d7043e9b6b907d72f9b63fa52813f04c67cfd56e
1ozturkbe <1ozturkbe@gmail.com>
2017-11-14 10:25:57 -0500

    Final fits for engine P/W

--- enginefit/fitEnginePvsW.py
+++ enginefit/fitEnginePvsW.py
@@ -2,11 +2,7 @@ import pandas as pd
 import numpy as np 
 from numpy import logspace, log, log10, amin, amax
 import matplotlib.pyplot as plt
-# you have to change it to this and run the file form gpfit
 from gpfit.fit import fit
-
-# since this is not an installable code source you have to navigate to the
-# ~gpfit folder.  
 
 # SOURCE OF DATA: http://media.aero.und.edu/uasresearch.org/documents/195-197_Reference-Section_Engines.pdf
 
@@ -16,13 +12,15 @@ P = df[u'37000'];
 minP = amin(P)
 maxP = amax(P)
 logP = log(P/maxP)
-indP0 = np.array(np.argpartition(logP, -2)[-2:])
+#indP0 = np.array(np.argpartition(logP, -2)[-2:])
+indP0 = []
 
 W = df[u'33.2'];
 minW = amin(W)
 maxW = amax(W)
 logW = log(W/maxW)
-indW0 = np.array([logW.argmax()])
+#indW0 = np.array([logW.argmax()])
+indW0 = []
 
 
 indices = np.delete(np.linspace(0,len(logP)-1,len(logP)),
@@ -34,16 +32,20 @@ Type = 'SMA'
 K = 2
 
 cstrt, rms_error = fit(logPin,logWin,K,Type)
-#Result: w**0.789 = 0.0336 * (u_1)**0.157
-#           + 1.57 * (u_1)**1.34
+
+#Result for K=1: w**0.1 = 0.984118 * (u_1)**0.117039
+# rms_error 0.412
+
+#Result for K=2: w**1.92 = 0.00441 * (u_1)**0.759
+#           + 1.44 * (u_1)**2.90
 # rms_error 0.346
 
 
 # Plotting data
 plt.plot(P[indices]/1000,W[indices],'r*') 
 Pplot = np.linspace(1,150000,100)
-plt.plot(Pplot/1000,(0.0336 * (Pplot/maxP)**0.157
-    + 1.57 * (Pplot/maxP)**1.34)**(1/0.789)*maxW,'b-')
+plt.plot(Pplot/1000, maxW*(0.00441* (Pplot/maxP)**0.759 +
+                           1.44* (Pplot/maxP)**2.90)**(1/1.92))
 plt.xlabel('Mean sea level maximum power (kW)')
 plt.ylabel('Engine weight (lbs)')
 plt.grid(True)
