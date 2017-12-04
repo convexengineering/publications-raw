c9bebce02d1563cff55b23684fdf5abb6049d376
1ozturkbe <1ozturkbe@gmail.com>
2017-12-04 14:55:35 -0500

    Reusing lapse rate fit.

--- enginefit/fitLapseRate.py
+++ enginefit/fitLapseRate.py
@@ -8,9 +8,9 @@ from gpfit.fit import fit
 
 df = pd.read_csv('DF35_maxPvh.csv')
 
-h = df['ft']/15000
+h = np.array(df['ft']/15000)
 #h = np.delete(h,[0])
-P = df['kW']
+P = np.array(df['kW'])
 #P = np.delete(P,[0])
 
 P_MSL = np.amax(df['kW'])
@@ -31,27 +31,27 @@ plt.ylabel('log(1-Pnorm)')
 K = 1
 Type = 'SMA'
 
-#cstrt, rms_error = fit(logh[1:-1],logL[1:-1],K,Type)
+cstrt, rms_error = fit(logh[1:-1],logL[1:-1],K,Type)
 
 # Doing the power curve fit
 
-RPM = df['RPM'] #RPM
-P = df['P'] #kW
-BSFC = df['BSFC'] #kg/kWhr
+# RPM = df['RPM'] #RPM
+# P = df['P'] #kW
+# BSFC = df['BSFC'] #kg/kWhr
+#
+# RPM = RPM[0:6]
+# P = P[0:6]
+# BSFC = BSFC[0:6]
+#
+# BSFCmin = min(BSFC)
+# RPMmax = max(RPM)
+# Pmax = max(P)
+#
+# BSFC = BSFC/BSFCmin
+# RPM = RPM/RPMmax
+# P = P/Pmax
 
-RPM = RPM[0:6]
-P = P[0:6]
-BSFC = BSFC[0:6]
 
-BSFCmin = min(BSFC)
-RPMmax = max(RPM)
-Pmax = max(P)
-
-BSFC = BSFC/BSFCmin
-RPM = RPM/RPMmax
-P = P/Pmax
-
-
-cstrt, rms_error = fit(log(RPM),log(P),K,Type)
+# cstrt, rms_error = fit(log(RPM),log(P),K,Type)
 
 
