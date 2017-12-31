d357b424862143f34df65d135b5026f89028a37a
1ozturkbe <1ozturkbe@gmail.com>
2017-12-31 08:59:23 -0600

    Multimission running robustly.

--- SimPleAC_multimission.py
+++ SimPleAC_multimission.py
@@ -20,28 +20,33 @@ class Multimission(Model):
             t_m     = Variable('t_m','hr','Total mission time')
 
             with Vectorize(Nsegments):
-                Wavg   = Variable('W_{avg}','N','Segment average weight')
-                Wstart = Variable('W_{start}', 'N', 'Weight at the beginning of flight segment')
-                Wend   = Variable('W_{end}', 'N','Weight at the end of flight segment')
-                h      = Variable('h','m','Flight altitude')
-                dhdt   = Variable('\\frac{dh}{dt}','m/hr','Climb rate')
-                W_f_s  = Variable('W_{f_s}','N', 'Segment fuel burn')
-                t_s    = Variable('t_s','hr','Time spent in flight segment')
-                R_s    = Variable('R_s','km','Range flown in segment')
-                state  = Atmosphere()
+                Wavg    = Variable('W_{avg}','N','segment average weight')
+                Wstart  = Variable('W_{start}', 'N', 'weight at the beginning of flight segment')
+                Wend    = Variable('W_{end}', 'N', 'weight at the end of flight segment')
+                h       = Variable('h','m','final segment flight altitude')
+                havg    = Variable('h_{avg}','m','average segment flight altitude')
+                dhdt    = Variable('\\frac{dh}{dt}','m/hr','climb rate')
+                W_f_s   = Variable('W_{f_s}','N', 'segment fuel burn')
+                t_s     = Variable('t_s','hr','time spent in flight segment')
+                R_s     = Variable('R_s','km','range flown in segment')
+                state   = Atmosphere()
                 self.aircraftP = self.aircraft.dynamic(state)
 
             # Mission variables
+            hcruise    = Variable('h_{cruise}', 5000, 'm', 'minimum cruise altitude')
             Range      = Variable("Range",3000, "km", "aircraft range")
             W_p        = Variable("W_p", 6250, "N", "payload weight", pr=20.)
             V_min      = Variable("V_{min}", 25, "m/s", "takeoff speed", pr=20.)
-            cost_index = Variable("Cost Index",360,'1/hr','hourly cost index')
+            cost_index = Variable("Cost Index",120,'1/hr','hourly cost index')
+            TOfac      = Variable('T/O factor', 2.,'-','takeoff thrust factor')
+
 
         constraints = []
 
         # Setting up the mission
         with SignomialsEnabled():
-            constraints += [h == state['h'], # Linking states
+            constraints += [havg == state['h'], # Linking states
+                        h[1:Nsegments-1] >= hcruise,  # Adding minimum cruise altitude
 
                         # Weights at beginning and end of mission
                         Wstart[0] >= W_p + self.aircraft.wing['W_w'] + self.aircraft.engine['W_e'] + W_f_m,
@@ -56,10 +61,14 @@ class Multimission(Model):
                         # Altitude changes
                         h[0] == t_s[0]*dhdt[0], # Starting altitude
                         dhdt >= 1.*units('m/hr'),
+                        havg[0] == 0.5*h[0],
+                        havg[1:Nsegments] == (h[1:Nsegments]*h[0:Nsegments-1])**(0.5),
                         SignomialEquality(h[1:Nsegments],h[:Nsegments-1] + t_s[1:Nsegments]*dhdt[1:Nsegments]),
 
+                        # Max MSL thrust at least 2*climb thrust
+                        self.aircraft.engine['P_{shaft,max}'] >= TOfac*self.aircraftP.engineP['P_{shaft}'][0],
                         # Thrust and fuel burn
-                        W_f_s >= self.aircraftP.engineP['BSFC'] * self.aircraftP.engineP['T'] * t_s,
+                        W_f_s >= self.aircraft['g'] * self.aircraftP.engineP['BSFC'] * self.aircraftP.engineP['P_{shaft}'] * t_s,
                         self.aircraftP.engineP['T'] * self.aircraftP['V'] >= self.aircraftP['D'] * self.aircraftP['V'] + Wavg * dhdt,
 
                         # Flight time
@@ -92,11 +101,11 @@ class Multimission(Model):
         return constraints, state, self.aircraft, self.aircraftP
 
 if __name__ == "__main__":
-    m = Multimission(3,5)
+    m = Multimission(2,5)
     m.substitutions.update({
-        'Range'     :[3000*units('km'), 2300*units('km'), 1500*units('km')],
-        'W_p'       :[6250*units('N'),   8000*units('N'),   4000*units('N')],
-        'Cost Index':[360*units('1/hr'), 480*units('1/hr'), 240*units('1/hr')]
+        'Range'     :[3000*units('km'), 1000*units('km')],
+        'W_p'       :[6250*units('N'),   8000*units('N')],
+        'Cost Index':[120*units('1/hr'), 360*units('1/hr')],
     })
     m.cost = m['W_{f_{mm}}']*units('1/N') + sum(m['Cost Index']*m['t_m'])
     sol = m.localsolve(verbosity = 4)
