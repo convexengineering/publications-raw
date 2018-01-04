96ce8def20abdcaa8fd4d1ada0d7d5f3a04633a6
1ozturkbe <1ozturkbe@gmail.com>
2018-01-04 10:24:36 -0600

    BSFC graph complete.

--- enginefit/fitBSFC.py
+++ enginefit/fitBSFC.py
@@ -30,12 +30,14 @@ Lnew = np.linspace(0.1,1,100)
 #newPoints = (0.992*(Lnew)**-0.0303)**10
 #newPoints = (2670*(Lnew)**32.7 + 0.925*(Lnew)**-0.140)**(1/0.373)
 newPoints = (.984*Lnew**-0.0346)**10
+newPoints2 = (.984*Lnew**-0.0346)**10
 for i in range(0,100):
-    if newPoints[i] <= 1.:
-        newPoints[i] = 1.
+    if newPoints2[i] <= 1.:
+        newPoints2[i] = 1.
 #plt.plot(L,BSFC/BSFCmin,label='data')
-plt.plot(P/Pmax,BSFC/BSFCmin,label='data')
+plt.plot(P/Pmax,BSFC/BSFCmin,label='data',marker='o')
 plt.plot(Lnew,newPoints,label='fit')
+plt.plot(Lnew,newPoints2,label='fit+bound')
 plt.xlabel('$P_{shaft}/P_{shaft,alt}$')
 plt.ylabel('$BSFC/BSFC_{min}$')
 plt.legend(loc=1)
